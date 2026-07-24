0f45a302ee7cb40af86337e61b9c1384d4d0a54b
Boakai Dorbor Kamara <boakaidkamara@gmail.com>
2026-07-24 00:34:46 +0000

    add other endpoints for creating, updating, and deleting user

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -4,6 +4,9 @@ const userController = require("../controllers/userController");
 const Router = express.Router();
 
 Router.get("/users", userController.getUser);
+Router.post("/users", userController.createUser);
+Router.patch("/users/:id", userController.updateUser);
+Router.delete("/users/:id", userController.deleteUser);
 
 
 module.exports = Router;
